d62d40d79d269df35515e79cd32cae44f4c12994
vikramch <chunduruvikram@gmail.com>
2024-11-03 08:37:22 +1100

    Changing assertion format as per OUTPUT_FORMAT in testCensusSingle_InvalidAge_ThrowsExceptionOrIgnores

--- src/test/java/TestCensus.java
+++ src/test/java/TestCensus.java
@@ -64,7 +64,7 @@ public class TestCensus {
             assertTrue("Iterator hasn't been closed.", iterator.closed);
             assertTrue("Invalid result null.", strings != null);
             System.out.println(Arrays.toString(strings));
-            assertArrayEquals(new String[]{"1:0:3", "2:1:2", "3:2:1"}, strings);
+            assertArrayEquals(new String[]{"1:0=3", "2:1=2", "3:2=1"}, strings);
         } catch (RuntimeException e) {
             System.out.println("Invalid ages throw exception. Not bad!");
         } finally {
